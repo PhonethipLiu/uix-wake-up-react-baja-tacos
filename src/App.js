73c62d27c006114e9dbbccc2a6259e420c0a1d53
Dylan B <giglett21@gmail.com>
2018-04-17 13:42:49 -0700

    added the header with react. The begining of greatness

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
-
-
+import Header from './components/header/header';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return(
+      <div className="navBar"> 
+      <Header title="Hustle" name="User"/>
       </div>
-    );
+    )
   }
-}
+};
+
 
 export default App;
