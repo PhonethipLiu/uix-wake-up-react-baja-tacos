7a629b1518708058278ad20a92f420453fb0d4eb
Dylan B <giglett21@gmail.com>
2018-04-18 08:42:10 -0700

    added function to change the zipcode. getWeather is now invokedand still need to creat the render function

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -5,27 +5,60 @@ let zipCode = 37221;
 class Weather extends Component {
     constructor(props) {
         super(props);
-    }
-};
 
-function getWeather() {
-    console.log("get weather");
-    fetch('https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=f5f69f7deb5aae2ed812df935759b130&units=imperial')
-    .then(res => res.json())
-    .then(
-        (result) => {
-            console.log("result:", result);
+        this.state = {
+            weatherLoaded:false,
+            objResult: {},
+            error: null,
+            modal:false
+        }
+    this.toggle = this.toggle.bind(this);
+    }
+    toggle(){
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+    
+      changeZipClick() {
+          console.log("change zip code");
+          zipCode = document.getElementById("zipCodeInput").value;
+          console.log("new zipcode:", zipCode);
             this.setState({
-                loadWeather: true,
-                objResult: result,
-                isHidden: true
-            });
-        },
-        (error) => {
-            console.log("error");
-            this.steState({
-                isLoaded: true,
-                error: error
-            });
-        })
- }
+                weatherLoaded: false,
+                objResult,
+                error: null,
+            }, this.setState({
+                modal: !this.statemodal
+            }), this.getWeather());
+      }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+        this.getWeather();
+    }
+
+
+
+    getWeather() {
+        console.log("get weather");
+        fetch('https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=f5f69f7deb5aae2ed812df935759b130&units=imperial')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log("result:", result);
+                this.setState({
+                    loadWeather: true,
+                    objResult: result,
+                    isHidden: true
+                });
+            },
+            (error) => {
+                console.log("error");
+                this.steState({
+                    isLoaded: true,
+                    error: error
+                });
+            })
+        }
+    };
