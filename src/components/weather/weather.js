459d6fecde0074deec6469f70c82a5ac1463ff58
Dylan B <giglett21@gmail.com>
2018-04-17 18:40:16 -0700

    added function to call to weather api. still need to invoke

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -8,3 +8,24 @@ class Weather extends Component {
     }
 };
 
+function getWeather() {
+    console.log("get weather");
+    fetch('https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=f5f69f7deb5aae2ed812df935759b130&units=imperial')
+    .then(res => res.json())
+    .then(
+        (result) => {
+            console.log("result:", result);
+            this.setState({
+                loadWeather: true,
+                objResult: result,
+                isHidden: true
+            });
+        },
+        (error) => {
+            console.log("error");
+            this.steState({
+                isLoaded: true,
+                error: error
+            });
+        })
+ }
