fa4b2ab83d9b355043140b7bf412fcde2cd98cde
Kelsey Pintens <kelseypintens@Kelseys-MacBook-Pro.local>
2018-04-19 07:45:59 -0500

    added music data and firebase config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header';
+import Song from './components/music/Music';
 
 class App extends Component {
   render() {
     return(
       <div className="navBar"> 
       <Header title="Hustle" name="User"/>
+      <Song />
       </div>
     )
   }
